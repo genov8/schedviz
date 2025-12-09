3a7dd8ba3e10e295fb711b7bf47634ba52ffea83
Sutemgenov Zhandos <genov8@Sutemgenovs-MacBook-Pro.local>
2025-12-09 16:46:44 +0500

    refactor: switch CLI to use pkg/schedviz API

--- cmd/schedviz/main.go
+++ cmd/schedviz/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/genov8/schedviz/pkg/schedviz"
+)
 
 func main() {
-	fmt.Println("scheduler viz")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		snap, err := schedviz.ParseLine(line)
+		if err != nil {
+			continue
+		}
+		if snap == nil {
+			continue
+		}
+
+		fmt.Printf(
+			"t=%4dms | P=%2d (idle=%2d) | M=%2d (idle=%2d) | GRQ=%4d | LRQ=%v\n",
+			snap.TimestampMs,
+			snap.Gomaxprocs,
+			snap.Idleprocs,
+			snap.Threads,
+			snap.IdleThreads,
+			snap.GlobalRunQueue,
+			snap.LocalRunQueues,
+		)
+	}
 }
